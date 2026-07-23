8c2c5ffd1e79e8a5cb2911457174c4a216dbb3b5
n6810210533 <6810210533@psu.ac.th>
2026-07-23 22:51:50 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+process.loadEnvFile();
+import express from 'express';
+
+const express = require('express')
+const app = express()
+app.listen(5000, () => {
+    console.log('Server running on port 5000 6810210533')
+})
